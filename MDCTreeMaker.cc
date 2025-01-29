4fdf6c8fd0444e45f18f18de8f4bada4b487ca54
emclaughlin2 <emlaugh1020@gmail.com>
2025-01-29 11:22:25 -0500

    State of simana0 when running over data for vz shift of 3cm systematic

--- MDCTreeMaker.cc
+++ MDCTreeMaker.cc
@@ -379,9 +379,9 @@ int MDCTreeMaker::process_event(PHCompositeNode *topNode)
           return Fun4AllReturnCodes::EVENT_OK;
         }
       if(_debug) cout << "about to get mbdvtx z value for mbdreco module with vertex pointer: " << mbdvtx << " type: " << typeid(mbdvtx).name() << endl;
-      track_vtx[2] = mbdvtx->get_z(); 
+      track_vtx[2] = mbdvtx->get_z() - 3.0; // EDITED FOR Z VERTEX RESOLUTION SYSTEMATIC
       if(_debug) cout << "got mbdvtx z value for mbdreco module" << endl;
-      if(track_vtx[2] == 0 || abs(track_vtx[2]) > 50 || isnan(track_vtx[2])) // is zero no longer the default mbd value for mbd reco 
+      if(track_vtx[2] == 0.00 || abs(track_vtx[2]) > 50 || isnan(track_vtx[2])) // is zero no longer the default mbd value for mbd reco 
         {
           if(_debug) cout << "Zero, nan or very large MBD vtx in MBDreco module - skipping event." << endl;
           return Fun4AllReturnCodes::EVENT_OK;
